6afeaa078dd9e7a24b497bf1a4831bd1447bba4c
seanc505 <seancahill15@gmail.com>
2024-05-16 23:49:43 +0100

    initial commit of hello world

--- 01_test/src/main.cpp
+++ 01_test/src/main.cpp
@@ -1,20 +1,19 @@
 #include <Arduino.h>
+#include <U8g2lib.h>
+#include <SPI.h>
 
-// Define the LED pin
-const int LED_PIN = 0;
+// U8G2 Constructor for SPI mode
+U8G2_ST7920_128X64_F_SW_SPI u8g2(U8G2_R0, /* clock=*/ 52, /* data=*/ 51, /* cs=*/ 53, /* reset=*/ 49);
 
 void setup() {
-  // Initialize the LED pin as an output
-  pinMode(LED_PIN, OUTPUT);
+  u8g2.begin();
 }
 
 void loop() {
-  // Turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_PIN, HIGH);
-  // Wait for a second
-  delay(500);
-  // Turn the LED off by making the voltage LOW
-  digitalWrite(LED_PIN, LOW);
-  // Wait for a second
-  delay(500);
+  u8g2.firstPage();
+  do {
+    u8g2.setFont(u8g2_font_ncenB08_tr);
+    u8g2.drawStr(0, 24, "Hello World!");
+  } while (u8g2.nextPage());
+  delay(1000);
 }
